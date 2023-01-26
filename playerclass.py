9df3d8991772ed09dad6a0d05cd99bcf38a6b962
Celene Saw <azadesu.github@gmail.com>
2023-01-26 16:21:41 +1100

    added Square, Player, Go, Property Class

--- playerclass.py
+++ playerclass.py
@@ -58,3 +58,76 @@ class Board:
             amounts.append(player.get)
 
 
+class Player:
+    def __init__(self, name, player_number):
+        self.name = name
+        self.player_number = player_number
+        self.amount = constants.STARTING_AMOUNT
+        self.properties_owned = defaultdict([])
+
+    def buy_property(self, property):
+        self.properties_owned[property.colour].append(property)
+
+    # defining getters and setters
+
+    def get_amount(self):
+        return self.amount
+
+    # Only amount and properties are changed, so let's write setter methods for those
+
+    def add_amount(self, num):
+        self.set_amount(self.get_amount() + num)
+
+
+class Square:
+    def __init__(self, name, square_type):
+        self.name = name
+        self.square_type = square_type
+
+    def is_bankrupt(player):
+        if (player.amount < 0):
+            return True
+        return False
+
+    def action(player: Player):
+        # empty
+        return
+
+
+class Go(Square):
+    def action(player: Player):
+        player.add_amount(1)
+
+
+class Property(Square):
+    def __init__(self, name, square_type, price, colour):
+        super(name, square_type)
+        self.price = price
+        self.colour = colour
+        self.owner = -1
+
+    # defining getters
+    # setters not allowed as properties do not change in the game
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        return self.price
+
+    def get_colour(self):
+        return self.colour
+
+    def get_square_type(self):
+        return self.square_type
+
+    def get_owner(self):
+        return self.square_type
+
+    def set_owner(self, owner_name):
+        self.owner = owner_name
+
+    def action(self, player: Player):
+        if self.owner == "":
+            player.buy_property()
+            self.set_owner(player.player_number)
+        return
