06dc2c002796709d5adbfa421c8e2ea4bb2301a0
Celene Saw <azadesu.github@gmail.com>
2023-01-26 16:31:58 +1100

    changed square list to dictionary type for consistency, added some getters for Board class

--- playerclass.py
+++ playerclass.py
@@ -5,22 +5,29 @@ import constants
 class Board:
     def __init__(self, raw_squares, raw_players):
         self.player_dict = self.parse_player_list(raw_squares)
-        self.square_list = self.parse_square_list(raw_players)
+        self.square_dict = self.parse_square_list(raw_players)
         print(self.player_dict)
         print(self.square_list)
 
         self.NUM_PLAYERS = len(self.player_dict)
-        self.NUM_SQUARES = len(self.square_list)
+        self.NUM_SQUARES = len(self.square_dict)
+
+    # defining getters
+    def get_players(self):
+        return self.player_dict
+
+    def get_squares(self):
+        return self.square_list
 
     # private method
     def parse_player_list(self, player_list):
         try:
+            new_dict = dict()
             if len(player_list) == 0:
-                return dict()
-            player_dict = dict()
+                return new_dict
             for i in range(len(player_list)):
-                player_dict[i] = Player(player_list[i], i)
-            return player_dict
+                new_dict[i] = Player(player_list[i], i)
+            return new_dict
         except:
             return
         finally:
@@ -29,18 +36,18 @@ class Board:
     # private method
     def parse_square_list(self, square_list):
         try:
-            new_list = list()
+            new_dict = dict
             if len(square_list) == 0:
-                return new_list
+                return new_dict
             for i in range(len(square_list)):
                 data = square_list[i]
                 if (data["type"] == "property"):
-                    new_list.append(
-                        Property(data["name"], data["price"], data["colour"], data["type"]))
+                    new_dict[i] = Property(
+                        data["name"], data["price"], data["colour"], data["type"])
                 if (data["type"] == "go"):
-                    new_list.append(
-                        Property(data["name"], data["price"], data["colour"], data["type"]))
-            return new_list
+                    new_dict[i] = Property(
+                        data["name"], data["price"], data["colour"], data["type"])
+                    return new_dict
         except:
             return
         finally:
@@ -52,7 +59,7 @@ class Board:
         print("What spaces does everybody finish on:")
 
     def get_winners(self):
-        players = get_players()
+        players = self.et_players()
         amounts = list()
         for player in players:
             amounts.append(player.get)
@@ -95,13 +102,16 @@ class Square:
 
 
 class Go(Square):
+    # def __init__(self, name, square_type):
+    #     super.__init__(name, square_type)
+
     def action(player: Player):
         player.add_amount(1)
 
 
 class Property(Square):
     def __init__(self, name, square_type, price, colour):
-        super(name, square_type)
+        super.__init__(name, square_type)
         self.price = price
         self.colour = colour
         self.owner = -1
