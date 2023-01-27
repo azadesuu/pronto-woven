bca13ac83b49c53dac573bd510806419f5d91986
Celene Saw <azadesu.github@gmail.com>
2023-01-27 11:49:10 +1100

    drafting tests code skeleton

--- tests/unittests.py
+++ tests/unittests.py
@@ -1,4 +1,18 @@
+from ..classes.board import Board
+from ..classes.player import Player
+from ..classes.square import Square, Property, Go
 import unittest
+
+
+class TestBoard():
+    def a():
+        pass
+
+
+class TestPlayer():
+    def a():
+        pass
+
 
 class TestSum(unittest.TestCase):
 
@@ -7,6 +21,7 @@ class TestSum(unittest.TestCase):
 
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
 
 if __name__ == '__main__':
     unittest.main()
